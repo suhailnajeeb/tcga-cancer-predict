af58d883bc4bff3be53f57c0be758bb48412834e
Suhail Najeeb <suhailnajeeb19@gmail.com>
2019-12-13 12:11:04 +0600

    update modelLib

---
+++ modelLib.py
@@ -0,0 +1,31 @@
+from tensorflow.keras.layers import Conv1D, Input, Dense, Dropout, MaxPooling1D, Flatten
+from tensorflow.keras.models import Model
+
+modelArch = {}												
+addModel = lambda f:modelArch.setdefault(f.__name__,f)
+
+# build and return model
+def makeModel(architecture,verbose=True):
+
+	model = modelArch[architecture]()
+
+	if verbose:
+		print(model.summary(line_length=150))
+	
+	return model
+
+@addModel
+def CNN1D_001(nFeat = 20530, n_classes = 33):
+    input = Input(shape = (nFeat,1))
+
+    x1 = Conv1D(filters = 64, kernel_size = 3, activation = 'relu') (input)
+    x2 = Conv1D(filters = 64, kernel_size = 3, activation = 'relu') (x1)
+    x3 = Dropout(0.5)(x2)
+    x4 = MaxPooling1D(pool_size = 2)(x3)
+    x5 = Flatten()(x4)
+    x6 = Dense(100, activation = 'relu')(x5)
+    out = Dense(n_classes, activation = 'softmax')(x6)
+
+    model = Model(inputs = input, outputs = out)
+
+    return model
