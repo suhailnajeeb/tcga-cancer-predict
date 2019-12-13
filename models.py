360083f5b495ff25b27aa5ef7e786e199e59893a
Suhail Najeeb <suhailnajeeb19@gmail.com>
2019-12-13 09:41:57 +0600

    bugfix

--- models.py
+++ models.py
@@ -1,13 +1,14 @@
 from tensorflow.keras.layers import Conv1D, Input, Dense, Dropout, MaxPooling1D
 
-input = Input(shape = (nFeat,))
+input = Input(shape = (nFeat,1))
 
 x1 = Conv1D(filters = 64, kernel_size = 3, activation = 'relu') (input)
 x2 = Conv1D(filters = 64, kernel_size = 3, activation = 'relu') (x1)
 x3 = Dropout(0.5)(x2)
-x4 = MaxPooling1D(pool_size = 2)
+x4 = MaxPooling1D(pool_size = 2)(x3)
 x5 = Flatten()(x4)
 x6 = Dense(100, activation = 'relu')(x5)
-out = Dense(n_classes, activation = 'softmax')
+out = Dense(n_classes, activation = 'softmax')(x6)
 
-model.compile(loss = 'categorical_crossentropy', optimizer='adam', metrics = ['accuracy'])
+model = Model(inputs = input, outputs = out)
+model.summary()
--- train.py
+++ train.py
@@ -16,10 +16,10 @@ set_session(tf.Session(config=config))
 
 dbPath = 'C:\Data\data.h5'
 
-weightsFolder = '\\weights\\'
+weightsFolder = 'weights\\'
 modelName = 'conv1D'
-bestModelPath = '\\weights\\best.hdf5'
-modelFolder = '\\model\\'
+bestModelPath = 'weights\\best.hdf5'
+modelFolder = 'model\\'
 
 epochs = 100
 epochStart = 0
@@ -48,15 +48,15 @@ test_generator = generator(db, X_test, batch_size = 32)
 from tensorflow.keras.layers import Conv1D, Input, Dense, Dropout, MaxPooling1D, Flatten
 from tensorflow.keras.models import Model
 
-input = Input(shape = (nFeat,))
+input = Input(shape = (nFeat,1))
 
 x1 = Conv1D(filters = 64, kernel_size = 3, activation = 'relu') (input)
 x2 = Conv1D(filters = 64, kernel_size = 3, activation = 'relu') (x1)
 x3 = Dropout(0.5)(x2)
-x4 = MaxPooling1D(pool_size = 2)
+x4 = MaxPooling1D(pool_size = 2)(x3)
 x5 = Flatten()(x4)
 x6 = Dense(100, activation = 'relu')(x5)
-out = Dense(n_classes, activation = 'softmax')
+out = Dense(n_classes, activation = 'softmax')(x6)
 
 model = Model(inputs = input, outputs = out)
 model.summary()
@@ -70,13 +70,13 @@ from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, CSVLogger
 
 check1 = ModelCheckpoint(os.path.join(weightsFolder, modelName + "_{epoch:02d}-loss-{val_loss:.3f}.hdf5"), monitor='val_loss', save_best_only=True, mode='auto')
 check2 = ModelCheckpoint(bestModelPath, monitor='val_loss', save_best_only=True, mode='auto')
-check3 = EarlyStopping(monitor='val_loss', min_delta=0.01, patience=patience, verbose=0, mode='auto')
-check4 = CSVLogger(os.path.join(modelFolder, modelName +'_trainingLog.csv'), separator=',', append=True)
-check5 = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=patience//1.5, verbose=1, mode='auto', epsilon=0.0001, cooldown=0, min_lr=1e-10)
+#check3 = EarlyStopping(monitor='val_loss', min_delta=0.01, patience=patience, verbose=0, mode='auto')
+#check4 = CSVLogger(os.path.join(modelFolder, modelName +'_trainingLog.csv'), separator=',', append=True)
+#check5 = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=patience//1.5, verbose=1, mode='auto', epsilon=0.0001, cooldown=0, min_lr=1e-10)
 
 
 trained_model = model.fit_generator(train_generator, steps_per_epoch=(len(X_train) // batchSize), epochs=epochs, initial_epoch=epochStart,
-											validation_data= test_generator, validation_steps=(len(X_test) // batchSize), callbacks=[check1,check2,check3,check4,check5], 
+											validation_data= test_generator, validation_steps=(len(X_test) // batchSize), callbacks=[check1,check2],#,check3,check4,check5], 
 											verbose=1)
 
 db.close()
